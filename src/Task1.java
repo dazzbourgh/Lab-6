841f0eebc83899e157c9fa65e419017bd2596f9d
Leonid Borisevich <leonidborisevich00@gmail.com>
2016-08-02 23:01:26 +0300

    no message

---
+++ src/Task1.java
@@ -0,0 +1,19 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Leonid on 02.08.2016.
+ */
+public class Task1 {
+    public static void main(String[] args){
+        try {
+            InputStream inputStream = new FileInputStream("");
+            DataInputStream in = new DataInputStream(inputStream);
+        }
+        catch(Exception e){
+
+        }
+    }
+}
